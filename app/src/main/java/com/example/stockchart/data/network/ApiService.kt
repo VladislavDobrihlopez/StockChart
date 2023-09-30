157289429fa3b2495ad18dd3688685db57468b04
Vladislav Voitov <dobrihlopez@gmail.com>
2023-09-30 13:10:11 +0300

    add zoom

--- app/src/main/java/com/example/stockchart/data/network/ApiService.kt
+++ app/src/main/java/com/example/stockchart/data/network/ApiService.kt
@@ -7,7 +7,7 @@ import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface ApiService {
-    @GET("aggs/ticker/{stock_name}/range/1/hour/2022-01-09/2023-01-09?adjusted=true&sort=asc&limit=50000&apiKey=${Configuration.API_KEY}")
+    @GET("aggs/ticker/{stock_name}/range/1/hour/2022-01-09/2023-01-09?adjusted=true&sort=desc&limit=50000&apiKey=${Configuration.API_KEY}")
 //    @GET("aggs/ticker/AAPL/range/1/hour/2022-01-09/2023-01-09?adjusted=true&sort=asc&limit=50000&apiKey=Is6w5efIZ0AYP5cf1wBPQCo4Fo4LXobp")
     suspend fun getStockBars(@Path("stock_name") name: String): ResponseDto
 }
--- app/src/main/java/com/example/stockchart/presentation/Chart.kt
+++ app/src/main/java/com/example/stockchart/presentation/Chart.kt
@@ -1,24 +1,51 @@
 package com.example.stockchart.presentation
 
 import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.gestures.TransformableState
+import androidx.compose.foundation.gestures.transformable
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import com.example.stockchart.data.network.dto.BarInfoDto
+import kotlin.math.roundToInt
+
+private const val DEFAULT_NUMBER_OF_VISIBLE_BARS = 80
+private const val DEFAULT_MIN_NUMBER_OF_VISIBLE_BARS = 20
+private const val DEFAULT_MAX_NUMBER_OF_VISIBLE_BARS = 1000
 
 @Composable
 fun Chart(stockBars: List<BarInfoDto>, modifier: Modifier = Modifier) {
-    Canvas(modifier = modifier) {
-        val widthDistance = size.width / stockBars.size
+    var visibleNumberOfBars by remember {
+        mutableStateOf(DEFAULT_NUMBER_OF_VISIBLE_BARS)
+    }
+
+    val transform = TransformableState(onTransformation = { zoomChange, _, _ ->
+        visibleNumberOfBars = (visibleNumberOfBars / zoomChange)
+            .roundToInt()
+            .coerceIn(DEFAULT_MIN_NUMBER_OF_VISIBLE_BARS, stockBars.size)
+    })
+
+    Canvas(modifier = modifier.transformable(transform)) {
+        val widthDistance = size.width / visibleNumberOfBars
         val maxCost = stockBars.maxOf { it.highest }
         val minCost = stockBars.minOf { it.lowest }
         val pxPerDollar = size.height / (maxCost - minCost)
-        stockBars.forEachIndexed { index, barInfoDto ->
+        stockBars.take(visibleNumberOfBars).forEachIndexed { index, barInfoDto ->
             drawLine(
                 color = Color.Yellow,
-                start = Offset(index * widthDistance, size.height - pxPerDollar * barInfoDto.lowest + pxPerDollar * minCost),
-                end = Offset(index * widthDistance, size.height - pxPerDollar * barInfoDto.highest + pxPerDollar * minCost),
+                start = Offset(
+                    size.width - index * widthDistance, // starting drawing from the right
+                    size.height - pxPerDollar * barInfoDto.lowest + pxPerDollar * minCost
+                ),
+                end = Offset(
+                    size.width - index * widthDistance,
+                    size.height - pxPerDollar * barInfoDto.highest + pxPerDollar * minCost
+                ),
             )
         }
     }
