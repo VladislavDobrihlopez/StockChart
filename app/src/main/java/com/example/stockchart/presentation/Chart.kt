233e122bdc1075eac76333186d44e8bd0e71a6c3
Vladislav Voitov <dobrihlopez@gmail.com>
2023-09-30 14:05:23 +0300

    add increase/decrease indicators and scroll

--- app/src/main/java/com/example/stockchart/presentation/Chart.kt
+++ app/src/main/java/com/example/stockchart/presentation/Chart.kt
@@ -10,8 +10,12 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.drawscope.translate
 import com.example.stockchart.data.network.dto.BarInfoDto
+import kotlin.math.abs
+import kotlin.math.max
 import kotlin.math.roundToInt
 
 private const val DEFAULT_NUMBER_OF_VISIBLE_BARS = 80
@@ -24,29 +28,67 @@ fun Chart(stockBars: List<BarInfoDto>, modifier: Modifier = Modifier) {
         mutableStateOf(DEFAULT_NUMBER_OF_VISIBLE_BARS)
     }
 
-    val transform = TransformableState(onTransformation = { zoomChange, _, _ ->
+    var scrolled by remember {
+        mutableStateOf(0f)
+    }
+
+    var screenSize by remember {
+        mutableStateOf(Size(0f, 0f))
+    }
+
+    var pxPerUnit by remember {
+        mutableStateOf(0f)
+    }
+
+    var barWidth by remember {
+        mutableStateOf(0f)
+    }
+
+    val transform = TransformableState(onTransformation = { zoomChange, panChange, _ ->
         visibleNumberOfBars = (visibleNumberOfBars / zoomChange)
             .roundToInt()
             .coerceIn(DEFAULT_MIN_NUMBER_OF_VISIBLE_BARS, stockBars.size)
+
+        scrolled = (scrolled + panChange.x).coerceIn(0f, barWidth * stockBars.size - screenSize.width)
     })
 
     Canvas(modifier = modifier.transformable(transform)) {
+        screenSize = size
         val widthDistance = size.width / visibleNumberOfBars
+        barWidth = widthDistance
         val maxCost = stockBars.maxOf { it.highest }
         val minCost = stockBars.minOf { it.lowest }
         val pxPerDollar = size.height / (maxCost - minCost)
-        stockBars.take(visibleNumberOfBars).forEachIndexed { index, barInfoDto ->
-            drawLine(
-                color = Color.Yellow,
-                start = Offset(
-                    size.width - index * widthDistance, // starting drawing from the right
-                    size.height - pxPerDollar * barInfoDto.lowest + pxPerDollar * minCost
-                ),
-                end = Offset(
-                    size.width - index * widthDistance,
-                    size.height - pxPerDollar * barInfoDto.highest + pxPerDollar * minCost
-                ),
-            )
+        pxPerUnit = pxPerDollar
+        translate(left = scrolled) {
+            stockBars.forEachIndexed { index, barInfoDto ->
+                drawLine(
+                    color = Color.Yellow,
+                    start = Offset(
+                        size.width - index * widthDistance, // starting drawing from the right
+                        size.height - pxPerDollar * barInfoDto.lowest + pxPerDollar * minCost
+                    ),
+                    end = Offset(
+                        size.width - index * widthDistance,
+                        size.height - pxPerDollar * barInfoDto.highest + pxPerDollar * minCost
+                    ),
+                )
+
+                drawRect(
+                    if (barInfoDto.open < barInfoDto.close) Color.Green else Color.Red,
+                    topLeft = Offset(
+                        size.width - index * widthDistance - widthDistance / 4,
+                        size.height - pxPerDollar * max(
+                            barInfoDto.open,
+                            barInfoDto.close
+                        ) + pxPerDollar * minCost
+                    ),
+                    size = Size(
+                        width = widthDistance / 2,
+                        height = pxPerDollar * abs(barInfoDto.open - barInfoDto.close)
+                    )
+                )
+            }
         }
     }
 }
